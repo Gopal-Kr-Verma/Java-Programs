1ecaadf8f9c809e4123b1405f02f9e4db7158d71
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-23 00:06:43 +0530

    Add files via upload

---
+++ 2_sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum
+{
+ public static void main(String[]a)
+  {
+   int b,c;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter two numbers");
+   b=sc.nextInt();
+   c=sc.nextInt();
+   int s=b+c;
+   System.out.println("sum="+s);
+  }
+}
