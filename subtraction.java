b1c1a512793365110f7d6a39d87fefd21bd86e6f
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-22 23:31:31 +0530

    Add files via upload

---
+++ subtraction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class subtraction
+{
+ public static void main(String[]a)
+  {
+   int b,c;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter two numbers");
+   b=sc.nextInt();
+   c=sc.nextInt();
+   int sbt=b-c;
+   System.out.println("subtraction="+sbt);
+  }
+}
