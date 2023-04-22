798c9baa228901916c27396c082052dc5f20bb7c
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-23 00:12:23 +0530

    Add files via upload

---
+++ 15_Display1To10.java
@@ -0,0 +1,16 @@
+/*write a program to display 1 to 10 numbers*/
+
+import java.util.*;
+class Display1To10
+{
+ public static void main(String[]a)
+  {
+   int i;
+   i=1;
+   while(i<=10)
+    {
+     System.out.println(i);
+     i++;
+    }
+  }
+}
