d7c34b5910c844a657277a45f53d9f409f17c724
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-23 00:07:23 +0530

    Add files via upload

---
+++ 4_multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class multiplication
+{
+ public static void main(String[]a)
+  {
+   int b,c;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter two numbers");
+   b=sc.nextInt();
+   c=sc.nextInt();
+   int m=b*c;
+   System.out.println("multiplication="+m);
+  }
+}
