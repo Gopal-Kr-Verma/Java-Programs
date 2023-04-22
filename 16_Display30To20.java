15b56b9bdcb1cc0088cdb4b5d7f0980a5c100548
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-23 00:12:42 +0530

    Add files via upload

---
+++ 16_Display30To20.java
@@ -0,0 +1,16 @@
+/* write a program to display 30 to 20 number */
+
+import java.util.*;
+class Display30To20
+{
+ public static void main(String[]a)
+  {
+   int i;
+   i=30;
+   while(i>=20)
+    {
+     System.out.println(i);
+     i--;
+    }
+  }
+}
