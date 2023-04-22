4379443b736caa48e487605f0a9428949b9c7962
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-23 00:07:42 +0530

    Add files via upload

---
+++ 5_square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class square
+{
+ public static void main(String[]a)
+  {
+   int n;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter a number");
+   n=sc.nextInt();
+   int sq=n*n;
+   System.out.println("square="+sq);
+  }
+}
